ade5a0cc7bd977aa0715ddad296dc728c3a8ad6b
Thomas Oltmann <thomas.oltmann.hhg@gmail.com>
2026-03-19 18:16:58 +0100

    More serial divider tests

--- test/module_tests/serial_div/test.py
+++ test/module_tests/serial_div/test.py
@@ -5,21 +5,13 @@ import cocotb
 from cocotb.clock import Clock
 from cocotb.triggers import ClockCycles
 
+WIDTH = 10
 
-@cocotb.test()
-async def test_project(dut):
-    WIDTH = 10
+async def check_division(dut, num, den):
+    dut._log.info(f"Divide {num} by {den}")
 
-    dut._log.info("Start")
-
-    # Set the clock period to 10 us (100 KHz)
-    clock = Clock(dut.clk, 10, unit="us")
-    cocotb.start_soon(clock.start())
-
-    dut._log.info("Test serial divider module")
-
-    dut.num.value = 100
-    dut.den.value = 10
+    dut.num.value = num
+    dut.den.value = den
 
     dut.load.value = 1
     await ClockCycles(dut.clk, 1)
@@ -28,4 +20,23 @@ async def test_project(dut):
     quo = dut.quo.value
     rem = dut.rem.value
 
-    assert (rem, quo) == (0, 10)
+    assert (quo, rem) == divmod(num, den)
+
+@cocotb.test()
+async def test_project(dut):
+    dut._log.info("Start")
+
+    # Set the clock period to 10 us (100 KHz)
+    clock = Clock(dut.clk, 10, unit="us")
+    cocotb.start_soon(clock.start())
+
+    await check_division(dut, 0, 1)
+    await check_division(dut, 1, 1)
+    await check_division(dut, 3, 2)
+    await check_division(dut, 100, 10)
+    await check_division(dut, 100, (1 << 10) - 1)
+    await check_division(dut, 991, 1)
+    await check_division(dut, 991, 2)
+    await check_division(dut, 991, 991)
+    await check_division(dut, 991, 990)
+    await check_division(dut, (1 << 10) - 1, (1 << 10) - 1)
